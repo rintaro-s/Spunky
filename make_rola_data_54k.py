70a34c49a030df7832cd737dc7c87598b0767572
rinta@24239 <120776920+rintaro-s@users.noreply.github.com>
2025-08-17 21:07:58 +0900

    次:LoRA

---
+++ make_rola_data_54k.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d581c4bad913a2aba6959d733dbf7c1908b8337a8c798bce815dd743c7bdb781
+size 7300
---
+++ trainTTS.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0823811107b2c004aeafd44b9a734fbf82d41b4e83505630c176f4e0eb02fb63
+size 2051
