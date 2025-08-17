817d07eff05d1e8a4177bc08e8c17544a8d0466c
rinta@24239 <120776920+rintaro-s@users.noreply.github.com>
2025-08-17 21:07:58 +0900

    次:LoRA

--- spunky/scripts/nicopedia.py
+++ spunky/scripts/nicopedia.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef3e9af83ae742cc0fa1cc528303d73c1b0af170d7374f95e2e530f6ddfd0574
-size 1714
+oid sha256:77ed9263663152f951773de74b16fc1f527369879fbb16d469e106cafeee2e70
+size 4003
