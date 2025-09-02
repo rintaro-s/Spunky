f2c43b0d1a769d07fb2dc7f02d193f6dd642de0c
rinta@24239 <120776920+rintaro-s@users.noreply.github.com>
2025-09-02 14:18:15 +0900

    modified:   client.py

--- client.py
+++ client.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1e0de88241b5d3d69452d86cf928f6a820dd5a5cd734be273be0911f3c75535
-size 7034
+oid sha256:a781d101084e6ce887010f8031cd64d0ffcf317dda7a40eae2616e4337897efd
+size 7021
