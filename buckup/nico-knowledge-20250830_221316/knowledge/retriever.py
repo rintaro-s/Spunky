e3d32913beb1aeb91a1a5e48235a905de2f56fe5
rinta@24239 <120776920+rintaro-s@users.noreply.github.com>
2025-09-02 14:14:29 +0900

    Remove sensitive datasets/checkpoints from repo and add to .gitignore

--- buckup/nico-knowledge-20250830_221316/knowledge/retriever.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e3793503375b9b8371672c816b073aefcc962ca11fba067b85a941ea841e5d09
-size 3177
--- buckup/nico-new/retriever.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b0a9a9cfd41133d482434a6ab6b7429856690aeee0f04b30f8070c68c58016a5
-size 2473
