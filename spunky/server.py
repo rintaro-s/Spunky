6d9be8ea14e5142e9239cbee5dc6b299066ee9ed
rinta@24239 <120776920+rintaro-s@users.noreply.github.com>
2025-09-08 19:34:37 +0900

    modified:   spunky/server.py

--- spunky/server.py
+++ spunky/server.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94d5dcd9ef1eec5eb9cc901299b641c51e8e5841dddc8222cebb536740faa60f
-size 6286
+oid sha256:f4df13173f407fe6bdc7b62ce7faae0955f3094a5491a43c016fb0f4222c1b44
+size 6282
